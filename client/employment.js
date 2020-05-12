9fae78f4c6bf752372bbd94eb12786adaae20c1d
Eric Feinstein <erichfeinstein@gmail.com>
2020-05-12 15:27:30 -0400

    compressed bg image, replaced resume

--- client/employment.js
+++ client/employment.js
@@ -2,8 +2,8 @@ export const employmentList = [
   {
     company: 'Cedrus Digital',
     description: [
-      `Worked extensively on enterprise web application for consulting firm's client, an international auto rental company, and building out pages for account management`,
-      'Created reusable and well-tested components using React, partnering with design team to develop efficient and responsive solutions for good user experience',
+      `Worked extensively on an enterprise web application for an international auto rental company, and built out pages for account management`,
+      'Created reusable and well-tested components using React, partnered with design team to develop efficient and responsive solutions for good user experience',
       'Leveraged cutting edge technologies, like GraphQL, React Hooks and React Native',
     ],
   },
--- public/bundle.js
+++ public/bundle.js
@@ -806,7 +806,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 var employmentList = exports.employmentList = [{
   company: 'Cedrus Digital',
-  description: ['Worked extensively on enterprise web application for consulting firm\'s client, an international auto rental company, and building out pages for account management', 'Created reusable and well-tested components using React, partnering with design team to develop efficient and responsive solutions for good user experience', 'Leveraged cutting edge technologies, like GraphQL, React Hooks and React Native']
+  description: ['Worked extensively on an enterprise web application for an international auto rental company, and built out pages for account management', 'Created reusable and well-tested components using React, partnered with design team to develop efficient and responsive solutions for good user experience', 'Leveraged cutting edge technologies, like GraphQL, React Hooks and React Native']
 }];
 
 /***/ }),
